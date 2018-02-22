2d078e171373e371e42226e1fbfc35c37255a05c
Joshua Wiens <jrwiens0707@gmail.com>
2018-02-22 13:27:42 -0600

    spin feature now works, some customizability added, continuing on to add button features

--- js-3dBox/script/3dBox.js
+++ js-3dBox/script/3dBox.js
@@ -1,8 +1,9 @@
 class ThreeDBox {
-    constructor(box,buttons){
+    constructor(box,buttons,initTime,incrementTime,spin,spinSpeed){
         this.isMobile = this.checkMobile();
         this._createCore(box,buttons);
         this._initStyles(box);
+        this._initProperties(box,initTime,incrementTime,spin,spinSpeed);
         ThreeDBox.attachListener(box.faces[0],"load",this._initGeometry);
         // box.centerElement = this.centerElement;
     }
@@ -61,9 +62,7 @@ class ThreeDBox {
             // console.log(box);
         }
     }
-    static attachListener(element,action,listenerFunction){
-        element.addEventListener(action,listenerFunction,false);
-    }
+
     _initStyles(box){
         var numFaces = box.faces.length;
         console.log(box);
@@ -72,17 +71,27 @@ class ThreeDBox {
         console.log(window.innerHeight);
         console.log(height);        
         box.style.position = "relative";
+        box.style.perspective = "200px";        
         box.rotator.style.position = "relative";
-        box.rotator.style.perspective = "200px";
         box.rotator.style.transformStyle = "preserve-3d";
         // box.rotator.style.display = "inline-block";        
         box.rotator.style.height = height+"px";
          for(var i=0;i<numFaces;i++){
             box.faces[i].style.position = "absolute";
             box.faces[i].style.height = height+"px";  
-            // box.faces[i].style.transformStyle = "preserve-3d"; 
+            box.faces[i].style.transformStyle = "preserve-3d"; 
         } 
     }
+    _initProperties(box,initTime,incrementTime,spin,spinSpeed){
+        box.initTime = initTime;
+        box.incrementTime = incrementTime;
+        box.spin = spin;
+        box.spinSpeed = spinSpeed;
+    }
+    static attachListener(element,action,listenerFunction){
+        element.addEventListener(action,listenerFunction,false);
+    }
+
     _initGeometry(evt){
         var box = evt.currentTarget.parentElement.parentElement, rotator = box.rotator, faces = box.faces, numFaces = faces.length;
         var maxWidth = 0;
@@ -96,33 +105,37 @@ class ThreeDBox {
         for(i=0;i<numFaces;i++){
             ThreeDBox.centerElement(faces[i]);
         }
-        ThreeDBox.init3D(faces,maxWidth);
+        ThreeDBox.init3D(box,rotator,faces,maxWidth);
     }
     static centerElement(element){
         var parent = element.parentElement, w = element.clientWidth, W = parent.clientWidth;
         element.style.left = ((W-w)/2)+"px";
     }
-    static init3D(faces,maxWidth){
+    static init3D(box,rotator,faces,maxWidth){
         var numFaces = faces.length, theta = 360/numFaces, origin = ThreeDBox.getOrigin((theta/2)*(Math.PI/180),maxWidth/2);
-        console.log(origin);
+        // console.log(origin);
         for(var i=0;i<numFaces;i++){
-            TweenMax.to(faces[i],2,{rotationY:(i*theta),transformOrigin:origin});
+            TweenMax.to(faces[i],box.initTime,{rotationY:(i*theta),transformOrigin:origin});
         }
-
+        box.origin = origin;
+        if(box.spin){
+            var spr = 60/box.spinSpeed;
+            TweenMax.to(rotator,spr,{rotationY:360,transformOrigin:origin,ease:Power0.easeNone}).repeat(-1).delay(box.initTime);
+        }
     }
     static getOrigin(phi,w){
-        var originZ = Math.trunc(-w/Math.tan(phi));
+        var originZ = -w/Math.tan(phi);
         var origin = "50% 50% " + originZ + "px";
         // console.log(origin);
         return origin;
     }
 
-    static initThreeDBoxes(selector,{buttons = "off"}={}){
+    static initThreeDBoxes(selector,{buttons = "off",initTime = 3,incrementTime = 1,spin = false, spinSpeed = 15}={}){
         var boxes = document.querySelectorAll(selector), numBoxes = boxes.length;
         var threeDBoxes = new Array(numBoxes);
         if(window.ThreeDBoxes == undefined){window.ThreeDBoxes = [];} 
         for(var i=0;i<numBoxes;i++){
-            threeDBoxes[i] = new ThreeDBox(boxes[i],buttons);
+            threeDBoxes[i] = new ThreeDBox(boxes[i],buttons,initTime,incrementTime,spin,spinSpeed);
             threeDBoxes[i].box = boxes[i];
             window.ThreeDBoxes.push(threeDBoxes[i]);
         }
